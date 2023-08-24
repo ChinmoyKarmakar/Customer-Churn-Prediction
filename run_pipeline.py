630308fbed851010949e5a85eed70497bc91e60a
Suryansh Gupta <suryanshgupta9933@gmail.com>
2023-08-25 05:25:14 +0530

    changed path

--- run_pipeline.py
+++ run_pipeline.py
@@ -5,4 +5,4 @@ from pipeline.train_pipeline import train_pipeline
 
 # Defining the main function
 if __name__ == '__main__':
-    train_pipeline(data_path="customer_churn_large_dataset_optimized.parquet")
+    train_pipeline(data_path="data/customer_churn_large_dataset_optimized.parquet")
