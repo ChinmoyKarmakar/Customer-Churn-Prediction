3f4520c5384201d60302ce3b125fb2aad73bb7e2
Suryansh Gupta <suryanshgupta9933@gmail.com>
2023-08-24 23:23:35 +0530

    main

---
+++ run_pipeline.py
@@ -0,0 +1,8 @@
+# Importing Dependencies
+import logging
+
+from pipeline.train_pipeline import train_pipeline
+
+# Defining the main function
+if __name__ == '__main__':
+    train_pipeline(data_path="customer_churn_large_dataset_optimized.parquet")
