b9d05471bde433148daa87d34bdce3400bfb662e
Suryansh Gupta <suryanshgupta9933@gmail.com>
2023-08-25 04:40:37 +0530

    updated training pipeline

--- pipeline/train_pipeline.py
+++ pipeline/train_pipeline.py
@@ -18,7 +18,7 @@ def train_pipeline(data_path: str):
     df = ingest_data(data_path)
     X_train, X_test, y_train, y_test = clean_data(df)
     model = train_model(X_train, y_train)
-    accuracy_score, precision_score, recall_score, f1_score = eval_model(model, X_test, y_test)
+    accuracy_score, precision_score, recall_score, f1_score, roc_auc_score = eval_model(model, X_test, y_test)
     print(f"Accuracy: {accuracy_score}")
     print(f"Precision: {precision_score}")
     print(f"Recall: {recall_score}")
