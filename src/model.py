f077f84b0f61b8f0866e4238423cf8d16ea9717a
Suryansh Gupta <suryanshgupta9933@gmail.com>
2023-08-25 19:22:59 +0530

    added predict proba

--- src/model.py
+++ src/model.py
@@ -3,7 +3,7 @@ import logging
 from abc import ABC, abstractmethod
 
 from sklearn.linear_model import LogisticRegression
-from joblib import dump
+from joblib import dump, load
 
 # Create a class for the Model
 class Model(ABC):
@@ -30,6 +30,38 @@ class Model(ABC):
             X_test (pd.DataFrame): Testing data
         Returns:
             y_pred: Predicted values
+        """
+        pass
+
+    @abstractmethod
+    def save(self, filename: str):
+        """
+        Save the model to a file
+        Args:
+            filename (str): Path to save the model
+        """
+        pass
+
+    @classmethod
+    @abstractmethod
+    def load(cls, filename: str):
+        """
+        Load the model from a file
+        Args:
+            filename (str): Path to load the model from
+        Returns:
+            model_instance: An instance of this class with the loaded model
+        """
+        pass
+
+    @abstractmethod
+    def predict_proba(self, X_test):
+        """
+        Predicts class probabilities
+        Args:
+            X_test (pd.DataFrame): Testing data
+        Returns:
+            y_proba: Predicted class probabilities
         """
         pass
 
@@ -96,4 +128,41 @@ class CustomLogisticRegressionModel(Model):
             logging.info(f"Model saved successfully to {filename}")
         except Exception as e:
             logging.error(f"Error saving the model: {e}")
+            raise e
+
+    @classmethod
+    def load(cls, filename: str):
+        """
+        Load the model from a file
+        Args:
+            filename (str): Path to load the model from
+        Returns:
+            model_instance: An instance of this class with the loaded model
+        """
+        try:
+            loaded_model = load(filename)
+            instance = cls()
+            instance.lr = loaded_model
+            logging.info(f"Model loaded successfully from {filename}")
+            return instance
+        except Exception as e:
+            logging.error(f"Error loading the model: {e}")
+            raise e
+
+    def predict_proba(self, X_test):
+        """
+        Predicts class probabilities
+        Args:
+            X_test (pd.DataFrame): Testing data
+        Returns:
+            y_proba: Predicted class probabilities
+        """
+        try:
+            # Predict class probabilities
+            y_proba = self.lr.predict_proba(X_test)
+
+            logging.info("Probability prediction completed successfully")
+            return y_proba
+        except Exception as e:
+            logging.error(f"Error in probability prediction: {e}")
             raise e
